a2a29b1e45a7185be504307cf9eaf9771e309fcc
Robert Davis <rtdavis22@gmail.com>
2011-12-16 00:00:23 -0500

    Added some prep file tests.

---
+++ test/prep_file_test.cc
@@ -0,0 +1,84 @@
+#include "gtest/gtest.h"
+#include "gmml/gmml.h"
+
+using namespace gmml;
+
+TEST(PrepFileTest, Constructor) {
+    PrepFile file("dat/test.prep");
+    PrepFile::const_iterator begin = file.begin();
+    EXPECT_EQ(begin->first, "PeA");
+    PrepFile::const_iterator end = file.end();
+    --end;
+    EXPECT_EQ(end->first, "PeA");
+    EXPECT_EQ(begin->second, end->second);
+}
+
+TEST(PrepFileTest, PrepFileResidue) {
+    PrepFile file("dat/test.prep");
+    PrepFile::const_iterator it = file.begin();
+    PrepFile::ResiduePtr residue = it->second;
+    EXPECT_EQ(residue->name, "PeA");
+    EXPECT_EQ(residue->coordinate_type, PrepFileResidue::kINT);
+    EXPECT_EQ(residue->output_format, PrepFileResidue::kFormatted);
+    EXPECT_EQ(residue->geometry_type, PrepFileResidue::kGeometryCorrect);
+    EXPECT_EQ(residue->dummy_atom_omission, PrepFileResidue::kOmit);
+    EXPECT_EQ(residue->dummy_atom_type, "DU");
+    EXPECT_EQ(residue->dummy_atom_position, PrepFileResidue::kPositionBeg);
+    EXPECT_EQ(residue->cutoff, -0.5820);
+    EXPECT_EQ(residue->atoms.size(), 21);
+    EXPECT_EQ(residue->loops.size(), 1);
+    EXPECT_EQ(residue->loops[0].from, 17);
+    EXPECT_EQ(residue->loops[0].to, 3);
+}
+
+TEST(PrepFileTest, FirstNonDummyAtom) {
+    PrepFile file("dat/test.prep");
+    PrepFile::const_iterator it = file.begin();
+    PrepFile::ResiduePtr residue = it->second;
+    PrepFileAtom *atom = residue->atoms[3];
+    EXPECT_EQ(atom->index, 4);
+    EXPECT_EQ(atom->name, "C1");
+    EXPECT_EQ(atom->type, "CG");
+    EXPECT_EQ(atom->topological_type, PrepFileAtom::kTopTypeM);
+    EXPECT_EQ(atom->bond_index, 3);
+    EXPECT_EQ(atom->angle_index, 2);
+    EXPECT_EQ(atom->dihedral_index, 1);
+    EXPECT_EQ(atom->bond_length, 1.4);
+    EXPECT_EQ(atom->angle, 113.3);
+    EXPECT_EQ(atom->dihedral, -60.0);
+    EXPECT_EQ(atom->charge, 0.48);
+}
+
+TEST(PrepFileTest, LastAtom) {
+    PrepFile file("dat/test.prep");
+    PrepFile::const_iterator it = file.begin();
+    PrepFile::ResiduePtr residue = it->second;
+    PrepFileAtom *atom = residue->atoms[3];
+    atom = residue->atoms[20];
+    EXPECT_EQ(atom->index, 21);
+    EXPECT_EQ(atom->name, "O6");
+    EXPECT_EQ(atom->type, "OS");
+    EXPECT_EQ(atom->topological_type, PrepFileAtom::kTopTypeM);
+    EXPECT_EQ(atom->bond_index, 9);
+    EXPECT_EQ(atom->angle_index, 7);
+    EXPECT_EQ(atom->dihedral_index, 6);
+    EXPECT_EQ(atom->bond_length, 1.413);
+    EXPECT_EQ(atom->angle, 112.7);
+    EXPECT_EQ(atom->dihedral, -56.7);
+    EXPECT_EQ(atom->charge, -0.451);
+}
+
+TEST(PrepFileSet, Constructor) {
+    PrepFileSet set;
+    PrepFile::iterator begin = set.begin();
+    PrepFile::iterator end = set.end();
+    bool same = (begin == end);
+    EXPECT_EQ(same, true);
+}
+
+TEST(PrepFileSet, Exists) {
+    PrepFileSet set;
+    set.load("dat/test.prep");
+    EXPECT_EQ(set.exists("PeA"), true);
+    EXPECT_EQ(set.exists("OME"), false);
+}
