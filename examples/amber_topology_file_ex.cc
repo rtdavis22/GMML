f1c4261e9f6434e79937ada178aba29454d8a64d
Robert Davis <rtdavis22@gmail.com>
2012-01-18 07:30:32 -0500

    Added some examples.

---
+++ examples/amber_topology_file_ex.cc
@@ -0,0 +1,34 @@
+// This is an example of how to create a simple topology file. Notice that the
+// format of the file has nothing to do with AMBER or the topology of anything.
+// It's just a particular way of representing data.
+// See include/gmml/internal/amber_top_file.h for documentation.
+
+#include <string>
+
+#include "gmml/gmml.h"
+
+using gmml::AmberTopFile;
+
+const char *kIntStrings[] = { "one", "two", "three", "four", "five",
+                              "six", "seven", "eight", "nine", "ten" };
+
+int main() {
+    AmberTopFile *file = new AmberTopFile;
+
+    AmberTopFile::SectionPtr string_section =
+            file->create_section("Int Names", "5a10");
+
+    AmberTopFile::SectionPtr int_section =
+            file->create_section("Radius", "10I4");
+
+    AmberTopFile::SectionPtr double_section =
+            file->create_section("Circumference", "5E12.2");
+
+    for (int i = 0; i < 10; i++) {
+        string_section->insert(std::string(kIntStrings[i]));
+        int_section->insert(i);
+        double_section->insert(2*3.1416*i);
+    }
+
+    file->print("geometry_fun.top");
+}
---
+++ examples/parameter_set_ex.cc
@@ -0,0 +1,108 @@
+// This program shows how to query a parameter set for force field information.
+// See include/gmml/internal/parameter_file.h for documentation.
+
+#include <cmath>
+
+#include <iostream>
+
+#include "gmml/gmml.h"
+
+using std::cout;
+using std::endl;
+
+using gmml::kNotSet;
+using gmml::ParameterFileAtom;
+using gmml::ParameterFileBond;
+using gmml::ParameterFileDihedral;
+using gmml::ParameterFileDihedralTerm;
+using gmml::ParameterFileImproperDihedral;
+using gmml::ParameterFileSet;
+
+void print_atom_type_info(const ParameterFileAtom *atom) {
+    cout << "Printing info for atom type " << atom->type << ":" << endl;
+
+    if (atom->mass == kNotSet) {
+        cout << " Mass not set." << endl;
+    } else {
+        cout << " Mass: " << atom->mass << endl;
+    }
+
+    if (atom->radius == kNotSet) {
+        cout << " Radius not set." << endl;
+    } else {
+        cout << " Radius: " << atom->radius << endl;
+    }
+
+    if (atom->well_depth == kNotSet) {
+        cout << " Well depth not set." << endl;
+    } else {
+        cout << " Well depth: " << atom->well_depth << endl;
+    }
+}
+
+void print_bond_type_info(const ParameterFileBond *bond) {
+    cout << "Printing info for bond type " << bond->types[0] << "-" <<
+           bond->types[1] << ":" << endl;
+
+    cout << " force constant: " << bond->force_constant << endl;
+
+    cout << " length: " << bond->length << endl;
+}
+
+void print_dihedral_term(const ParameterFileDihedralTerm *term) {
+    cout << "factor: " << term->factor <<
+            ", force constant: " << term->force_constant <<
+            ", phase: " << term->phase <<
+            ", periodicity: " << std::abs(term->periodicity) << endl;
+}
+
+void print_dihedral_type_info(const ParameterFileDihedral *dihedral) {
+    cout << "Printing info for dihedral type " << dihedral->types[0] << "-" <<
+            dihedral->types[1] << "-" << dihedral->types[2] << "-" <<
+            dihedral->types[3] << ":" << endl;
+
+    for (int i = 0; i < dihedral->terms.size(); i++) {
+        cout << " ";
+        print_dihedral_term(&dihedral->terms[i]);
+    }
+
+    if (dihedral->scee != kNotSet) {
+        cout << " scee: " << dihedral->scee << endl;
+    }
+
+    if (dihedral->scnb != kNotSet) {
+        cout << " scnb: " << dihedral->scnb << endl;
+    }
+}
+
+int main() {
+    ParameterFileSet *parmset = new ParameterFileSet;
+
+    parmset->load("dat/parm99.dat");
+    parmset->load("dat/Glycam_06h.dat");
+
+    const ParameterFileAtom *atom = parmset->lookup("CG");
+    if (atom == NULL) {
+        cout << "Atom type not found." << endl;
+    } else {
+        print_atom_type_info(atom);
+    }
+    cout << endl;
+
+    const ParameterFileBond *bond = parmset->lookup("H2", "CG");
+    if (bond == NULL) {
+        cout << "Bond type not found." << endl;
+    } else {
+        print_bond_type_info(bond);
+    }
+    cout << endl;
+
+    const ParameterFileDihedral *dihedral =
+            parmset->lookup("SM", "CG", "CG", "H1");
+    if (dihedral == NULL) {
+        cout << "Dihedral type not found." << endl;
+    } else {
+        print_dihedral_type_info(dihedral);
+    }
+    cout << endl;
+}
