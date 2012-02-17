fb906d3925d332ac9cc5159f4b5042c06961ffde
Robert Davis <rtdavis22@gmail.com>
2012-02-17 16:01:53 -0500

    Clean up coordinate file implementation.

--- include/gmml/internal/coordinate_file.h
+++ include/gmml/internal/coordinate_file.h
@@ -4,66 +4,75 @@
 #define GMML_INTERNAL_COORDINATE_FILE_H_
 
 #include <iosfwd>
+#include <memory>
 #include <string>
-#include <vector>
 
-#include "gmml/internal/geometry.h"
 #include "gmml/internal/stubs/common.h"
 
 namespace gmml {
 
+class Coordinate;
+
 // This class represents an AMBER restart file.
 // See http://ambermd.org/formats.html#restart for the file specification.
-//
-// TODO: pimplize this class.
 class CoordinateFile {
   public:
-    CoordinateFile() : noncoordinate_count_(0) {}
+    // The number of places after the decimal point.
+    static const int kCoordinatePrecision = 7;
 
-    CoordinateFile(const std::string& file_name) : noncoordinate_count_(0) {
-        read(file_name);
-    }
-
-    virtual ~CoordinateFile() {}
-
-    void print(const std::string& file_name) const;
-    void print() const;
-
-    size_t size() const { return coordinates_.size(); }
-
-    const Coordinate& operator[](int i) const { return coordinates_[i]; }
-
-    // The only difference between a coordinate and a "noncoordinate" is that
-    // noncoordinates aren't included in the total at the top of the file.
-    void add_coordinate(const Coordinate& c) { coordinates_.push_back(c); }
-    void add_noncoordinate(double x, double y, double z);
-
-    int coordinate_count() const {
-        return coordinates_.size() - noncoordinate_count_;
-    }
-
-  private:
+    // The number of characters each number occupies, including padding.
     static const int kCoordinateWidth = 12;
 
-    void read(std::istream& in);
-    void read(const std::string& file_name);
-    Status process_crd_line(const std::string& line);
-    Status append_coordinate(const std::string& crd);
+    // The number of characters the count label occupies, including padding.
+    static const int kCountLabelWidth = 5;
+
+    static const char *kDefaultTitle;
+
+    // Creates an empty coordinate file with the default title.
+    CoordinateFile();
+
+    // Creates a coordinate file from the name of file on disk.
+    // throws std::invalid_argument
+    explicit CoordinateFile(const std::string& file_name);
+
+    virtual ~CoordinateFile();
+
+    // Writes the coordinate file to a given file.
+    void print(const std::string& file_name) const;
+
+    // Writes the coordinate file to standard output.
+    void print() const;
 
     void write(std::ostream& out) const;
-    void write_coordinate(std::ostream& out, const Coordinate& c) const;
 
-    std::string title_;
-    std::vector<Coordinate> coordinates_;
-    int noncoordinate_count_;
+    // Returns the total number of entries (triples) in the file.
+    // size() == coordinate_count() + noncoordinate_count()
+    size_t size() const;
+
+    // Returns the number of coordinates minus the number of "noncoordinates".
+    // 0 <= coordinate_count() <= size()
+    int coordinate_count() const { return size() - noncoordinate_count(); }
+
+    // Returns the number of "noncoordinates". See below.
+    // 0 <= noncoordinate_count() <= size()
+    int noncoordinate_count() const;
+
+    // 0 <= index < size()
+    // throws std::range_error
+    const Coordinate& operator[](int index) const;
+
+    void add_coordinate(const Coordinate& coordinate);
+
+    // A "noncoordinate" is a triple that's not included in the total at the
+    // top of the file.
+    void add_noncoordinate(double x, double y, double z);
+
+  private:
+    struct Impl;
+    std::auto_ptr<Impl> impl_;
 
     DISALLOW_COPY_AND_ASSIGN(CoordinateFile);
 };
-
-inline void CoordinateFile::add_noncoordinate(double x, double y, double z) {
-    coordinates_.push_back(Coordinate(x, y, z));
-    noncoordinate_count_++;
-}
 
 }  // namespace gmml
 
