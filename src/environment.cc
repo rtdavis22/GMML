044ff64429fbf906f494bab9dbfc50dd8d3b8a16
Robert Davis <rtdavis22@gmail.com>
2012-02-17 16:01:14 -0500

    Fix missing return bug.

--- src/environment.cc
+++ src/environment.cc
@@ -95,7 +95,7 @@ Residue *build_prep_file(const string& prep_code,
                          const Environment& environment) {
     const PrepFileSet *prep_files = environment.prep_files();
     if (prep_files->exists(prep_code)) {
-        build_prep_file((*prep_files)[prep_code]);
+        return build_prep_file((*prep_files)[prep_code]);
     }
     return NULL;
 }
